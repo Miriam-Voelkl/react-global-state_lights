c35e2fd32998c8859bdd003e37ac826a90afa479
Miriam Völkl <miriamvoelkl@gmail.com>
2024-01-18 12:05:23 +0100

    uplift state to app

--- pages/rooms.js
+++ pages/rooms.js
@@ -1,12 +1,16 @@
 import Link from "../components/Link";
 import Lights from "../components/Lights";
 
-export default function Rooms({ lights, toggleLight }) {
+export default function Rooms({ lights, toggleLight, handleToggle }) {
   return (
     <>
       <Link href="/">← Back home</Link>
       <h1>All Rooms</h1>
-      <Lights lights={lights} toggleLight={toggleLight} />
+      <Lights
+        lights={lights}
+        toggleLight={toggleLight}
+        handleToggle={handleToggle}
+      />
     </>
   );
 }
